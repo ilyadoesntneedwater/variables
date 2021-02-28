287f1f28420848ec383177c666a243f4498d842d
ilyadoesntneedwater <79465185+ilyadoesntneedwater@users.noreply.github.com>
2021-02-28 10:07:34 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Программа__в_которой_есть_переменные
+{
+
+
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = (Convert.ToInt32(textBox2.Text) * Convert.ToInt32(textBox1.Text)).ToString();
+        }
+    }
+}
+
